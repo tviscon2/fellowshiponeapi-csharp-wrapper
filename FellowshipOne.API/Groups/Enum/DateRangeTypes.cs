9abca52ba6b76736745561f328f5818976a42d2a
Tracy Mazelin <tracy.mazelin@activenetwork.com>
2014-06-17 14:55:17 -0500

    groups realm enums

---
+++ FellowshipOne.API/Groups/Enum/DateRangeTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FellowshipOne.API.Groups.Enum {
+    public enum DateRangeTypes {
+        Unspecified = 0,
+        Ticks = 1,
+        Milliseconds = 2,
+        Seconds = 3,
+        Days = 4,
+        Weeks = 5,
+        Months = 6,
+        Years = 7,
+        DOB = 8,
+    }
+}
